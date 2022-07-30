fc7fd46b42bd02e8ebbd28079b92737c239ca5a3
baejeonghyeon <84851488+baejeonghyeon@users.noreply.github.com>
2022-07-30 18:40:36 +0900

    add items

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,63 +1,63 @@
 import React from "react";
-import './Detail.css';
+import './Background.css';
 
-function Detail(props){  
-  console.log(props);
-
-  return(
+const Background = () => {
+  return (
     <React.Fragment>
-      <div className="pimg1">
-        <div className="ptext">
-          <span className="border">
+      <div class="pimg1">
+        <div class="ptext">
+          <span class="border">
             MOVIE SITE
           </span>
         </div>
       </div>
 
-      <section className="section section-light">
+      <section class="section section-light">
         <h2>Section One</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis provident ducimus quae incidunt sit, fugit minima eum accusantium illo impedit excepturi eligendi cumque, nemo repellendus facilis doloremque, explicabo sint et! Dignissimos quis maiores fugit nostrum illum labore voluptate hic, ea aspernatur voluptatem amet esse quod nemo rem adipisci! Itaque molestiae at, est laboriosam exercitationem nam explicabo! Vitae fugit error accusantium eum numquam dicta doloremque consectetur eveniet. Corporis, non nobis explicabo fugiat ab omnis itaque exercitationem enim recusandae voluptatum inventore ipsa quasi nemo officiis consectetur. Et, consequuntur corrupti itaque libero blanditiis omnis officia ullam, eveniet veniam natus nulla ex magnam quis.</p>
       </section>
 
-      <div className="pimg2">
-        <div className="ptext">
-          <span className="border trans">
+      <div class="pimg2">
+        <div class="ptext">
+          <span class="border trans">
             Image Two Text
           </span>
         </div>
       </div>
 
-      <section className="section section-dark">
+      <section class="section section-dark">
         <h2>Section Two</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis provident ducimus quae incidunt sit, fugit minima eum accusantium illo impedit excepturi eligendi cumque, nemo repellendus facilis doloremque, explicabo sint et! Dignissimos quis maiores fugit nostrum illum labore voluptate hic, ea aspernatur voluptatem amet esse quod nemo rem adipisci! Itaque molestiae at, est laboriosam exercitationem nam explicabo! Vitae fugit error accusantium eum numquam dicta doloremque consectetur eveniet. Corporis, non nobis explicabo fugiat ab omnis itaque exercitationem enim recusandae voluptatum inventore ipsa quasi nemo officiis consectetur. Et, consequuntur corrupti itaque libero blanditiis omnis officia ullam, eveniet veniam natus nulla ex magnam quis.</p>
       </section>
 
 
 
-      <div className="pimg3">
-        <div className="ptext">
-          <span className="border trans">
+      <div class="pimg3">
+        <div class="ptext">
+          <span class="border trans">
             Image Three Text
           </span>
         </div>
       </div>
 
-      <section className="section section-dark">
+      <section class="section section-dark">
         <h2>Section Three</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis provident ducimus quae incidunt sit, fugit minima eum accusantium illo impedit excepturi eligendi cumque, nemo repellendus facilis doloremque, explicabo sint et! Dignissimos quis maiores fugit nostrum illum labore voluptate hic, ea aspernatur voluptatem amet esse quod nemo rem adipisci! Itaque molestiae at, est laboriosam exercitationem nam explicabo! Vitae fugit error accusantium eum numquam dicta doloremque consectetur eveniet. Corporis, non nobis explicabo fugiat ab omnis itaque exercitationem enim recusandae voluptatum inventore ipsa quasi nemo officiis consectetur. Et, consequuntur corrupti itaque libero blanditiis omnis officia ullam, eveniet veniam natus nulla ex magnam quis.</p>
       </section>
 
-      <div className="pimg1">
-      
-        <div className="ptext">
-          <span className="border">
-            FIND OUT YOUR MOVIE
-          </span>
-        </div>
+      <div class="pimg1">
+        <a href="https://www.skku-startup-gru.net/">
+          <div class="ptext">
+            <span class="border">
+              FIND OUT YOUR MOVIE
+            </span>
+          </div>
+
+        </a>
 
       </div>
     </React.Fragment>
   )
 }
 
-export default Detail;
+export default Background;
--- src/routes/About.js
+++ src/routes/About.js
@@ -5,7 +5,7 @@ function About(){
   return(
     <div className="about__container">
       <span>
-        'Freedom is the freedom to say that two plus two make four. If that is granted, all else follow'
+        '공부하기 싫다'
       </span>
       <span>Bae Jeong Hyeon, 1998</span>
     </div>
